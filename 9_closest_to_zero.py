e1cad140436b76cdbd7a16e4a7b16b8d56800c72
Vignesh <46440651+Vignesh29071998@users.noreply.github.com>
2019-02-12 15:42:49 +0530

    Update 9_closest_to_zero.py

--- 9_closest_to_zero.py
+++ 9_closest_to_zero.py
@@ -1,8 +1,13 @@
+#hello
 n=int(input())
 n1=list(map(int,input().split()))
+m=max(n1)
+a,b=0,0
 for i in range(0,len(n1)-1):
   for j in range(i+1,len(n1)):
-    if n1[i]+n1[j]==0:
-      print(n1[i],n1[j])
-      break
+    if abs(n1[i]+n1[j])<m:
+      a,b=n1[i],n1[j]
+      m=abs(a+b)
+print(a,b)
+
       
