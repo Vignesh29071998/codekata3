1f88918de5e985848be9000c6036c81943143ab3
Vignesh <46440651+Vignesh29071998@users.noreply.github.com>
2019-02-16 18:35:17 +0530

    Create 47_remove_extra_spaces.py

---
+++ 47_remove_extra_spaces.py
@@ -0,0 +1,19 @@
+a=list(input())
+s=''
+i=0
+while i<len(a):
+  if a[i]==' ':
+    s+=a[i]
+    for j in range(i+1,len(a)):
+      if a[j]==' ':
+        i=j+1
+      else:
+        break
+  else:
+    s+=a[i]
+    i+=1
+print(s)
+        
+        
+      
+  
