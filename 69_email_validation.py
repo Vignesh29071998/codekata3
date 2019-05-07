99b5fea2fb3e407c6e19f2eac4e3731c35653baa
Vignesh <46440651+Vignesh29071998@users.noreply.github.com>
2019-05-07 21:20:39 +0530

    Create 69_email_validation.py

---
+++ 69_email_validation.py
@@ -0,0 +1,19 @@
+import collections
+s=input()
+b={}
+if s[-4:]=='.com':
+  b=collections.Counter(s)
+  if b['@']!=1 or b['@']!=0 and b['.']!=1:
+    print('NO')
+  else:
+    id1=s.index('@')
+    id2=s.index('.')
+    if len(s[id1+1:id2])<3:
+      print('NO')
+    else:
+      if len(s[:id1])<2:
+        print('NO')
+      else:
+        print('YES')
+else:
+  print('NO')
