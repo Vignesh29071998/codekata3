3e264daebc2046a23fcf78908d15a6a70dab7e25
Vignesh <46440651+Vignesh29071998@users.noreply.github.com>
2019-05-14 16:34:09 +0530

    Update 72_reverse_odd_positions.py

--- 72_reverse_odd_positions.py
+++ 72_reverse_odd_positions.py
@@ -1,10 +1,12 @@
 a = list(input().split())
-b = []
+b,c = [],''
 for i in range(0,len(a)):
   if i%2==0:
-    c = a[i]
+    for j in a[i]:
+      if j != '.':
+        c +=j
     b.append(c[::-1])
+    c = ''
   else:
     b.append(a[i])
 print(*b)
-
