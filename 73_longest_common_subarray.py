270dc2b71ce296fc051ce77099db43587dc819c8
Vignesh <46440651+Vignesh29071998@users.noreply.github.com>
2019-05-14 19:53:51 +0530

    Create 73_longest_common_subarray.py

---
+++ 73_longest_common_subarray.py
@@ -0,0 +1,18 @@
+s = input()
+t = input()
+a,b,s1,t1,m = [],[],'','',0
+for i in range(0,len(s)-1):
+  for j in range(i,len(s)):
+    s1 = s1+s[j]
+    a.append(s1)
+  s1 = ''
+for i in range(0,len(t)-1):
+  for j in range(i,len(t)):
+    t1 = t1+t[j]
+    b.append(t1)
+  t1 = ''
+for i in a:
+  if i in b and len(i)>m:
+    m = len(i)
+    k = i
+print(k)
