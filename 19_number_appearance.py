e0e928d237c7bdca2c44d10e650d14bb53f7371f
Vignesh <46440651+Vignesh29071998@users.noreply.github.com>
2019-05-15 00:28:57 +0530

    Update 19_number_appearance.py

--- 19_number_appearance.py
+++ 19_number_appearance.py
@@ -3,15 +3,18 @@ n,k = map(int,input().split())
 a,b,c = [],[],[]
 for i in range(0,n):
   a.append(list(map(int,input().split())))
-for i in range(0,n-1):
-  for j in range(0,k):
-    for l in range(0,k):
-      if a[i][j]==a[i+1][l]:
-        b.append(a[i][j])
-        break
-d = collections.Counter(b)
-for i in b:
-  if i not in c:
-    if d[i]==n-1:
-      c.append(i)
-print(*sorted(c))
+if n==1 and k==1:
+  print('1')
+else:
+  for i in range(0,n-1):
+    for j in range(0,k):
+      for l in range(0,k):
+        if a[i][j]==a[i+1][l]:
+          b.append(a[i][j])
+          break
+  d = collections.Counter(b)
+  for i in b:
+    if i not in c:
+      if d[i]==n-1:
+        c.append(i)
+  print(*sorted(c))
