65a6d7e79a40ce0424d19dfc33bb73a3d68a2fa3
Vignesh <46440651+Vignesh29071998@users.noreply.github.com>
2019-05-25 12:13:20 +0530

    Create 101_sum_of_three_primes.py

---
+++ 101_sum_of_three_primes.py
@@ -0,0 +1,24 @@
+n = int(input())
+a,flag,indic = [],0,0
+for i in range(2,n):
+  for j in range(2,i):
+    if i%j == 0:
+      flag = 0
+      break
+    else:
+      flag = 1
+  if i == 2:
+    a.append(i)
+  if flag == 1:
+    a.append(i)
+for i in range(0,len(a)-2):
+  for j in range(i+1,len(a)-1):
+    for k in range(j,len(a)):
+      if a[i]+a[j]+a[k] == n:
+        print(a[i],a[j],a[k])
+        indic = 1
+        break
+    if indic == 1:
+      break
+  if indic == 1:
+    break
