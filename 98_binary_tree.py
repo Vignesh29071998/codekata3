c0f083d4cad2dd47568a56e21f39297cd88f763b
Vignesh <46440651+Vignesh29071998@users.noreply.github.com>
2019-05-25 10:42:48 +0530

    Update 98_binary_tree.py

--- 98_binary_tree.py
+++ 98_binary_tree.py
@@ -2,6 +2,8 @@ n,m = map(int,input().split())
 a,c,flag = [],1,0
 for i in range(0,m):
   a.append(list(map(int,input().split())))
+if m == 0:
+  print('YES')
 for i in range(0,len(a)-1):
   for j in range(i+1,len(a)):
     if a[i][0] == a[j][0]:
