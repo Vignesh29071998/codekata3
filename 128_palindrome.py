c238142d292362a197668e9d3615b21423757abd
Vignesh <46440651+Vignesh29071998@users.noreply.github.com>
2019-05-27 20:46:47 +0530

    Update 128_palindrome.py

--- 128_palindrome.py
+++ 128_palindrome.py
@@ -11,13 +11,8 @@ for i in range(0,len(s)-1):
     if l != '' and l not in a:
       a.append(l)
     c,l = 0,''
-for i in range(0,len(a)):
-    for j in range(i+1,len(a)):
-        if len(a[i])==len(a[j]):
-            if a[i][0] > a[j][0]:
-                a[i],a[j] = a[j],a[i]
-        elif len(a[i]) > len(a[j]):
-            a[i],a[j] = a[j],a[i]
+a = sorted(a)
 for i in a:
     print(i)
+            
             
