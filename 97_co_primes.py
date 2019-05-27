a85a3c66b6559ed958769cea8c5772b1ba7d2252
Vignesh <46440651+Vignesh29071998@users.noreply.github.com>
2019-05-28 01:12:19 +0530

    Create 97_co_primes.py

---
+++ 97_co_primes.py
@@ -0,0 +1,19 @@
+n,m = map(int,input().split())
+def prime(a):
+  flag = 0
+  if a == 2:
+    flag = 1
+  else:
+    for i in range(2,a+1):
+      for j in range(2,i):
+        if i%j == 0:
+          flag = 0
+          break
+        else:
+          flag = 1
+  return flag
+a,b =prime(n),prime(m)
+if a==b==1:
+  print('yes')
+else:
+  print('no')
